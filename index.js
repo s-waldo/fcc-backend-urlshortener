bd09dc2e36ac1f00b6021338b1db9e64fd8dfb1c
s-waldo <stephen.waldo.94@gmail.com>
2024-10-08 16:13:39 -0500

    First Attempt

--- index.js
+++ index.js
@@ -1,24 +1,69 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+const dns = require("dns");
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
+mongoose.connect(process.env.MONGO_URI, {dbName: "fcc-backend", })
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+const urlSchema = new mongoose.Schema({
+  original_url: String,
+})
+
+urlSchema.plugin(AutoIncrement, {inc_field: 'short_url'})
+
+const Url = mongoose.model('Urls', urlSchema)
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+
+app.post("/api/shorturl", (req, res) => {
+  const isUrl = verifyUrl(req.body.url);
+  if (!isUrl) {
+    res.json({ error: "invalid url" });
+    return;
+  }
+  const cleanedUrl = urlCleanup(req.body.url);
+  dns.lookup(cleanedUrl, async (err, address) => {
+    if (err) {
+      res.json({ error: "invalid url" });
+      return;
+    }
+    const url = new Url({original_url: req.body.url})
+    await url.save()
+    res.json({original_url: url.original_url, short_url: url.short_url})
+  });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:id", async (req, res) => {
+  const url = await Url.findOne({short_url: req.params.id})
+  res.redirect(url.original_url)
+});
+
+const verifyUrl = (url) => {
+  const regex = /^(http|https):\/\/(?:www\.)?[a-zA-Z0-9]+\.[a-z]{2,6}$/;
+  return regex.test(url);
+};
+
+const urlCleanup = (url) => {
+  let temp = url.split(/[\/.]/);
+  if (temp.length >= 2) {
+    temp = temp[temp.length - 2] + "." + temp[temp.length - 1];
+  }
+  return temp;
+};
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
